a53cdb3ca8745121a51c36ad383cd93ca66e2cce
yash agrawal <agarwalshivam834@gmail.com>
2025-11-11 03:28:48 +0530

    add server

--- go.mod
+++ go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/golang/mock v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/robfig/cron/v3 v3.0.0 // indirect
 	github.com/twilio/twilio-go v1.28.5 // indirect
 )
--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron/v3"
 	"github.com/twilio/twilio-go"
 	api "github.com/twilio/twilio-go/rest/api/v2010"
 )
 
-func main() {
+func init() {
 	godotenv.Load(".env")
+}
+func scheduleCall() {
+	fmt.Println("scheduleCall")
 	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
 	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
 
@@ -20,7 +26,7 @@ func main() {
 	})
 
 	params := &api.CreateCallParams{}
-	params.SetTwiml("<Response><Say>Please take your medicine</Say></Response>")
+	params.SetTwiml("<Response><Say>Please take your medicines and fruits</Say></Response>")
 	params.SetTo("+918770798425")
 	params.SetFrom("+12513136008")
 	params.SetStatusCallback("http://localhost:3000/call-res")
@@ -38,4 +44,27 @@ func main() {
 			fmt.Println(resp.Sid)
 		}
 	}
+}
+
+func main() {
+	c := cron.New()
+	fmt.Println("time: ", time.Now().Format("2006-01-02 15:04:05"))
+	c.AddFunc("15 12 * * *", scheduleCall)
+	c.Start()
+	fmt.Println("Cron started")
+
+	s := &http.Server{
+		Addr: ":3000",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println("Hello, World!")
+			scheduleCall()
+		}),
+	}
+
+	err := s.ListenAndServe()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println("Server started on port 3000")
 }
